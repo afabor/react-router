183b34e9d08f1243c5badf5611e3ffe3ffae9e9f
Your Name <you@example.com>
2024-03-27 10:55:44 +0000

    tic tac toe in progress, squares added to board

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import "./App.css";
 import MyButton from "./components/MyButton/MyButton.jsx";
 import Profile from "./components/NavBar/Profile.jsx";
 import Counter from "./components/Counter/counter.jsx";
-import Square from "./components/TicTacToe/Square.jsx";
+import Board from "./components/TicTacToe/Board.jsx";
 
 function App() {
   return (
     <>
-      <Square />
+      <Board />
     </>
   );
 }
---
+++ src/components/TicTacToe/Board.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Board.css";
+import Square from "../Square/Square.jsx";
+
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
+}
